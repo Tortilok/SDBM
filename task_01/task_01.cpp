aee978d0bf5c0d5eb9cfcf6e871c5d6052ade4d6
Tortik <domashkin2120@gmail.com>
2021-10-11 13:49:05 +0300

    #1 issue Закончил программу
    1. Добавил создание массива по заданному размеру.
    2. Сделал заполнение пвсевдослучайными числами.
    3. Сделал вывод максимального значения элемента массива.

--- task_01/task_01.cpp
+++ task_01/task_01.cpp
@@ -1,6 +1,41 @@
 ﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    random_device rd;
+    mt19937_64 mersenne(rd());
+
+    int size;
+    unsigned int max;
+    cin >> size;
+    if (size > 1)
+    {
+       unsigned int* mas = new unsigned int[size];
+        mas[0] = mersenne();
+        max = mas[0];
+        cout << "Origin array:\n" << "mas[0]= " << mas[0] << "\n";
+        
+        for (int i = 1; i < size; i++)
+        {
+            mas[i] = mersenne();
+            cout << "mas[" << i << "]= " << mas[i] << "\n";
+            if (mas[i] > max)
+            {
+                max = mas[i];
+            }
+
+        }
+        cout << "Maximum value in array= " << max << "\n";
+        delete[] mas;
+    }
+    else
+    {
+        cout << "Еhe size of the array must be greater > 1\n";
+    }
+    system("pause");
+    return 0;
 }
