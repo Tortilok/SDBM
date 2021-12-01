154013cd60b2099e21f2619923b7089e1040495a
Tortilok <domashkin2120@gmail.com>
2021-12-01 16:05:06 +0300

    Переделал работу!!!!!!

--- task_01/task_01.cpp
+++ task_01/task_01.cpp
@@ -3,35 +3,44 @@
 
 using namespace std;
 
+unsigned int maximum(unsigned int array[], int size) {
+    unsigned int max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int output(unsigned int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "array[" << i << "]= " << array[i] << "\n";
+    }
+    return 0;
+}
+
+int fill_array(unsigned int array[], int size) {
+    random_device rd;
+    mt19937_64 mersenne(rd());
+    for (int i = 0; i < size; i++) {
+        array[i] = mersenne();
+    }
+    return 0;
+}
 
 int main()
 {   
-    random_device rd;
-    mt19937_64 mersenne(rd());
-
-    int size;
-    unsigned int max;
     cout << "Input size of array:\n";
+    int size;
     cin >> size;
     if (size > 1)
     {
-       unsigned int* mas = new unsigned int[size];
-        mas[0] = mersenne();
-        max = mas[0];
-        cout << "Origin array:\n" << "mas[0]= " << mas[0] << "\n";
-        
-        for (int i = 1; i < size; i++)
-        {
-            mas[i] = mersenne();
-            cout << "mas[" << i << "]= " << mas[i] << "\n";
-            if (mas[i] > max)
-            {
-                max = mas[i];
-            }
-
-        }
-        cout << "Maximum value in array= " << max << "\n";
-        delete[] mas;
+        unsigned int* array = new unsigned int[size];
+        fill_array(array, size);
+        output(array, size);
+        cout << "Maximum value in array= " << maximum(array, size) << "\n";
+        delete[] array;
     }
     else
     {
