155e73fec7520e883e5cdcf56d9695d979996555
Tortilok <domashkin2120@gmail.com>
2021-12-06 11:42:04 +0300

    Исправления замечаний по коду

--- task_01/task_01.cpp
+++ task_01/task_01.cpp
@@ -2,6 +2,69 @@
 #include <random>
 
 using namespace std;
+/**
+ * @brief Search MAXIMUM in array
+ * 
+ * @param array[]  Массив 
+ * @param size  Размер массива
+ * 
+ * 
+ */
+unsigned int maximum(unsigned int array[], const int size);
+/**
+ * @brief Вывод массива
+ *
+ * @param array[]  Массив
+ * @param size  Размер массива
+ *
+ *
+ */
+int output(unsigned int array[], const int size);
+/**
+ * @brief Заполнение массива псевдослучаныйми числами
+ *
+ * @param array[]  Массив
+ * @param size  Размер массива
+ *
+ *
+ */
+int fill_array(unsigned int array[], const int size); 
+/**
+ * @brief Проверка ввода размера массива
+ *
+ * @param string  Сообщение которое хотим вывести перед вводом размера
+ *
+ *
+ */
+size_t get_size(const string& message);
+
+
+
+int main()
+{   
+    try
+    {
+        int size = get_size("Input size for array:\n");
+        unsigned int* array = new unsigned int[size];
+        fill_array(array, size);
+        output(array, size);
+        cout << "Maximum value in array= " << maximum(array, size) << "\n";
+
+        if (array != nullptr) {
+            delete[] array;
+            array = nullptr;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        cout << "Exception: " << e.what();
+    }
+    
+        
+
+    system("pause");
+    return 0;
+}
 
 unsigned int maximum(unsigned int array[], int size) {
     unsigned int max = array[0];
@@ -12,8 +75,8 @@ unsigned int maximum(unsigned int array[], int size) {
     }
     return max;
 }
-
-int output(unsigned int array[], int size) {
+int output(unsigned int array[], int size)
+{
     for (int i = 0; i < size; i++) {
         cout << "array[" << i << "]= " << array[i] << "\n";
     }
@@ -29,23 +92,16 @@ int fill_array(unsigned int array[], int size) {
     return 0;
 }
 
-int main()
-{   
-    cout << "Input size of array:\n";
-    int size;
+size_t get_size(const string& message)
+{
+    int size = -1;
+    cout << message;
     cin >> size;
-    if (size > 1)
+
+    if (size < 0)
     {
-        unsigned int* array = new unsigned int[size];
-        fill_array(array, size);
-        output(array, size);
-        cout << "Maximum value in array= " << maximum(array, size) << "\n";
-        delete[] array;
+        throw out_of_range("Incorrect size. Value has to be greater or equal zero.\n");
     }
-    else
-    {
-        cout << "Еhe size of the array must be greater > 1\n";
-    }
-    system("pause");
-    return 0;
+
+    return size;
 }
